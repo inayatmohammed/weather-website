9750624981e509a0a8e2b4aaecab5ec202cd0d46
itachi <itachilord01@gmail.com>
2021-09-15 14:06:46 +0530

    init commit

---
+++ public/js/app.js
@@ -0,0 +1,40 @@
+console.log('Client side javascript file is loaded!')
+
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message1')
+const messageTwo = document.querySelector('#message2')
+
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    messageOne.textContent = 'loading....'
+    messageTwo.textContent = ''
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+        // try {
+        //     console.log(data)
+        // } catch (error) {
+        //     console.log('errorrrrr')
+        // }
+        if (data.error) {
+            console.log(data.error)
+            messageTwo.textContent = data.error
+        } else {
+            console.log(data.location)
+            console.log(data.forecast)
+            messageOne.textContent = data.location
+            messageTwo.textContent =  data.forecast
+
+        }
+        
+    })
+})
+    
+})
